e200d2cea0551bc02c589b5b98b2f0e27eef4e23
Evannnne <prefsuite@Live.ca>
2022-10-20 15:24:22 -0700

    checkpoint 2

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float movementSpeed = 16;
+    public float jumpVelocity = 10;
+    public float health = 100;
+
+    private SpriteRenderer m_spriteRenderer;
+    private Rigidbody2D m_rigidbody;
+    private Animator m_animator;
+
+    private bool m_canBeDamaged = true;
+
+    void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,6 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+           
     }
 }
